3da3cd0bf0458b0c40b4313f0e7c8f5b1c9c549e
Ana Ruiz Tovar <anapowa@gmail.com>
2021-06-15 14:09:36 -0400

    Refactored maze.js, added paragraph to restart game, updated CSS.

--- maze.js
+++ maze.js
@@ -1,6 +1,7 @@
 //Referenced code from Randy's demo from 25JUN2020, 29JUN2020, 
 // Study Hall with coach Marcus Chiriboga on 29JUN2020 and
 // Study Hall with coach Nikal Morgan on 30JUN2020. 
+
 const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W   W     W     W W W",
@@ -39,20 +40,19 @@ const createMaze = function (blueprint) {
                     blockDivs += "<div class='block wall'></div>"
                     break;
                 case "S":
-                    blockDivs += '<div id="cell' + colNum + "-" + rowNum + '" class="block start" >START<img id="link" src="link_nes.png"></img></div>'
+                    blockDivs += `<div id="cell${colNum}-${rowNum}" class="block start" >START<img id="link" src="link_nes.png"></img></div>`
                     y = rowNum
                     x = colNum
                     break;
                 case "F":
-                    blockDivs += '<div id="cell' + colNum + '-' + rowNum + '" class="block finish">FINISH<img id="triforce" src="240px-Triforce_Logo.png"></img></div>'
+                    blockDivs += `<div id="cell${colNum}-${rowNum}" class="block finish">FINISH<img id="triforce" src="240px-Triforce_Logo.png"></img></div>`
                     break;
                 default:
-                    blockDivs += '<div id="cell' + colNum + '-' + rowNum + '" class="block"></div>'
+                    blockDivs += `<div id="cell${colNum}-${rowNum}" class="block"></div>`
             }   
         }
 
     mazeEl.innerHTML += `<div class="row">${blockDivs}</div>`
-    // mazeEl.innerHTML += '<div class="row">' + blockDivs + '</div>'
     }
 }
 
@@ -64,15 +64,16 @@ let position = {
     linkTopPos: 270,
     linkLeftPos: 0
 }
-let linkInCell = x + "-" + y
 
- function moveLink(evt) {
+let linkInCell = `${x}-${y}`
+
+function moveLink(evt) {
     switch (evt.code){
         case "ArrowDown":
              if(map[y+1][x]!=="W"){
                 console.log(map[y][x])
                 position.linkTopPos += 30
-                linkInCell = x + "-" + (y+1)
+                linkInCell = `${x}-${y+1}`
                 y += 1
                 break; 
             } else {break}
@@ -81,16 +82,18 @@ let linkInCell = x + "-" + y
             if(map[y-1][x]!=="W"){
                 console.log(map[y][x])
                 position.linkTopPos -= 30
-                linkInCell = x + "-" + (y-1)
+                linkInCell = `${x}-${y-1}`
                 y -= 1
                 break;
             } else { break}
 
         case "ArrowLeft":
-            if(map[y][x-1]!=="W"){
+            if (linkInCell === `0-${y}`) {
+                break;
+            } else if(map[y][x-1]!=="W"){
                 console.log(map[y][x])
                 position.linkLeftPos -= 30
-                linkInCell = (x-1) + "-" + y
+                linkInCell = `${x-1}-${y}`
                 x -= 1
                 break;
             } else { break}
@@ -99,23 +102,12 @@ let linkInCell = x + "-" + y
             if(map[y][x+1]!=="W"){
                 console.log(map[y][x])
                 position.linkLeftPos += 30;
-                linkInCell = (x+1) + "-" + y;
+                linkInCell = `${x+1}-${y}`
                 x += 1
                 break;
             }else { break}
-        }
-    
-        const mazeBoundary = function () {            
-            if (linkInCell === "-1-"+y){
-                    position.linkLeftPos="0-9"
-                    console.log(map[y][x])
-                    position.linkLeftPos = 0
-                    inkInCell = (x+1) + "-" + y
-                    x+=1
-            }
-        }
-        mazeBoundary()
-        
+    }        
+
 
     document.getElementById("link").style.top = position.linkTopPos + "px";
     document.getElementById("link").style.left = position.linkLeftPos + "px";
@@ -128,46 +120,26 @@ let linkInCell = x + "-" + y
     const finishDiv = document.getElementById("cell20-8")
     const startDiv = document.getElementById("cell0-9")
     const link = document.getElementById("link")
+    const main = document.querySelector("main")
+    const restart = document.querySelector("#restart")
+    const body = document.querySelector("body")
 
         
     const grabTriforce = function (){
         if(map[y][x]==="F"){
             console.log("You won!!!")
             finishDiv.replaceChild(linkTriforceImg,triforce);
-            let removeLink = startDiv.removeChild(link)
+            startDiv.removeChild(link)
+            main.style.opacity = "0.25"
+            restart.innerHTML = "Play map again!"
+            body.addEventListener("click", () => {
+                window.location.reload();
+            })
+            body.classList.add('pointer');
         }
     }
-grabTriforce()
+    grabTriforce()
 
     console.log(linkInCell)
 }
 
-
-/* REFACTORING? */
-/* let rowNum
-let colNum
-let blockDivs
-let rowStr = ""
-let blockDivs =""
-
-const mazeEl = document.getElementById("maze")
-
-
-const createBlock = function (){
-    const blockType = rowStr[colNum]
-        switch (blockType){
-            case "W":
-                blockDivs += "<div class='block wall'></div>"
-                break;
-            case "S":
-                blockDivs += "<div class='block'><div id='start'>START</div></div>"
-                y = rowNum
-                x = colNum
-                break;
-            case "F":
-                blockDivs += "<div class='block' id='finish'></div>"
-                break;
-            default:
-                blockDivs += "<div class='block'></div>"
-        }
-    } */
